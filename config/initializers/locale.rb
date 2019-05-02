a1619ea404ac5a426c149aad51dfb06427211b7b
UsaMomokawa <spcmomo@gmail.com>
2019-05-03 07:33:19 +0900

    rubocopを導入した

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,3 @@
+# frozen_string_literal: true
+
 Rails.application.config.i18n.default_locale = :ja
--- db/migrate/20190502134534_add_user_ref_to_works.rb
+++ db/migrate/20190502134534_add_user_ref_to_works.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserRefToWorks < ActiveRecord::Migration[5.2]
   def change
     add_reference :works, :user, foreign_key: true
