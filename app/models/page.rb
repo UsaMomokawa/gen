8bb1bf73fa6292345eef247241da8155453dc83d
UsaMomokawa <spcmomo@gmail.com>
2019-05-20 00:37:56 +0900

    Pageモデルを作成した

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,11 +1,3 @@
-# frozen_string_literal: true
-
 class Page < ApplicationRecord
-  belongs_to :stage
-
-  enum status: {
-    not_complete: 0,
-    started: 1,
-    complete: 2
-  }
+  belongs_to :work
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_073302) do
+ActiveRecord::Schema.define(version: 2019_05_19_141212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pages", force: :cascade do |t|
+    t.boolean "matter", default: false, null: false
+    t.bigint "work_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["work_id"], name: "index_pages_on_work_id"
+  end
 
   create_table "stages", force: :cascade do |t|
     t.string "name", null: false
@@ -41,5 +49,6 @@ ActiveRecord::Schema.define(version: 2019_05_11_073302) do
     t.index ["user_id"], name: "index_works_on_user_id"
   end
 
+  add_foreign_key "pages", "works"
   add_foreign_key "works", "users"
 end
