74511f2a5d567865f18cacf2ca51a5a4a9eb302d
UsaMomokawa <spcmomo@gmail.com>
2019-05-21 23:08:15 +0900

    合計ページのカラムを作成

--- db/migrate/20190521111430_change_total_page_column_for_works.rb
+++ db/migrate/20190521111430_change_total_page_column_for_works.rb
@@ -1,5 +1,5 @@
 class ChangeTotalPageColumnForWorks < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :works, :total_page, false
+    change_column_null :works, :total_page, false, 4
   end
 end
