7eaa3c8bd2327659995d3bb1a07362d8e7efe1f3
UsaMomokawa <spcmomo@gmail.com>
2019-05-21 23:05:23 +0900

    新しい手順を登録できるようにした

--- app/controllers/works/stages_controller.rb
+++ app/controllers/works/stages_controller.rb
@@ -1,7 +1,37 @@
 class Works::StagesController < ApplicationController
   def show
-    @work = Work.find(params[:work_id])
+    @work  = find_work
     @stage = Stage.find(params[:id])
     @progresses = @work.progresses.where(stage: @stage)
   end
+
+  def new
+    @stage = Stage.new
+  end
+
+  def create
+    @stage = Stage.new(stage_params)
+    @work  = find_work
+    @stage.work_id = @work.id
+
+    if @stage.save!
+      @work.pages.each do |page|
+        Progress.create(work: @work, stage: @stage, page: page)
+      end
+      redirect_to work_stage_path(work_id: @work, id: @stage), notice: "#{@stage.name}が登録されました"
+    else
+      render "new"
+    end
+  end
+
+  private
+    def stage_params
+      params.require(:stage).permit(
+        :name
+      )
+    end
+
+    def find_work
+      Work.find(params[:work_id])
+    end
 end
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Stage < ApplicationRecord
-  belongs_to :stage
+  belongs_to :work
   has_many :progresses
 
   validates :name, presence: true, length: { maximum: 8 }
