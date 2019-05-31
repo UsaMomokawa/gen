f5dea90714833a64cd1855781cf96193816b48ff
UsaMomokawa <spcmomo@gmail.com>
2019-05-31 16:40:12 +0900

    ページを増やせるようにした

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  def create
+    work = Work.find(params[:work_id])
+    page = Page.new(
+      work: work
+    )
+    page.save!
+    work.stages.each do |stage|
+      Progress.create(work: work, stage: stage, page: page)
+    end
+    redirect_to work,
+      notice: "ページを追加しました"
+  end
 end
