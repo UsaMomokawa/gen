e0fdfe5661fdbab4feff2032bb34b9d3570b9c8e
UsaMomokawa <spcmomo@gmail.com>
2019-06-09 21:42:09 +0900

    pageシステムスペックを書いた

---
+++ spec/factories/stages.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :stage do
+  end
+end
--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -1,12 +1,22 @@
 FactoryBot.define do
   factory :work do
     title { "テストの原稿" }
-    user { FactoryBot.create :user }
+    user { create :user }
     deadline { Date.current }
 
     trait(:with_pages) do
       after(:create) do |work, evaluator|
         create_list(:page, 4, work: work)
+      end
+    end
+
+    trait(:with_stages) do
+      after(:create) do |work, evaluator|
+        pages = create_list(:page, 4, work: work)
+        stage = create(:stage, name: "線画", work: work)
+        pages.each do |page|
+          create(:progress, work: work, stage: stage, page: page)
+        end
       end
     end
   end
